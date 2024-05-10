94cfbfef58a224b8f2cad69dfe571dcba24aaaa3
PC <PC@INFO5-2561>
2024-05-10 09:26:27 +0200

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace EsercizioAS2324._3G.GuardabascioRiccardo.PSDBPCTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
