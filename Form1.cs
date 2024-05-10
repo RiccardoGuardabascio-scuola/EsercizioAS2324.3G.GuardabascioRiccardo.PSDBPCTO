17ab38ea70963fbb3faceeb76797ef5d5dbb17ea
PC <PC@INFO5-2561>
2024-05-10 09:51:17 +0200

    feat: ordinamento gare per punteggio minore o uguale di 3

--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,7 @@ namespace EsercizioAS2324._3G.GuardabascioRiccardo.PSDBPCTO
             string strConnessione = "";
             strConnessione = "Data Source=" + @"C:\Users\PC\Desktop\EsercizioAS2324.3G.GuardabascioRiccardo.PSDBPCTO\DB\MotoGP.db" + ";Version=3;";
             DataTable dtDati = new DataTable();
-            query = "SELECT * FROM Piloti ORDER BY Nome, Cognome";
+            query = "SELECT * FROM Piloti WHERE Nazionalita ORDER BY Nome, Cognome ";
 
             SQLiteDataAdapter da = new SQLiteDataAdapter(query, strConnessione);
             try
@@ -31,14 +31,14 @@ namespace EsercizioAS2324._3G.GuardabascioRiccardo.PSDBPCTO
                 MessageBox.Show(exe.Message);
             }
         }
-
-        private void btnGare_Click(object sender, EventArgs e)
+        private void btnGare_Click_1(object sender, EventArgs e)
         {
             string query = "";
             string strConnessione = "";
             strConnessione = "Data Source=" + @"C:\Users\PC\Desktop\EsercizioAS2324.3G.GuardabascioRiccardo.PSDBPCTO\DB\MotoGP.db" + ";Version=3;";
             DataTable dtDati = new DataTable();
-            query = "SELECT * FROM Gare";
+            query = "SELECT * FROM Gare WHERE Punteggio <=3 ";
+
             SQLiteDataAdapter da = new SQLiteDataAdapter(query, strConnessione);
             try
             {
@@ -49,11 +49,6 @@ namespace EsercizioAS2324._3G.GuardabascioRiccardo.PSDBPCTO
             {
                 MessageBox.Show(exe.Message);
             }
-        }
-
-        private void btnGare_Click_1(object sender, EventArgs e)
-        {
-
         }
     }
 }
