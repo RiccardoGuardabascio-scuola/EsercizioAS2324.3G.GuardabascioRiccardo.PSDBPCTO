5aa159ebab17ced79e0366950115b0d5be34622a
PC <PC@INFO5-2561>
2024-05-10 09:42:52 +0200

    feat: ordinamento piloti per nome e cognome

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            dgvDati = new DataGridView();
+            btnPiloti = new Button();
+            btnGare = new Button();
+            toolTip1 = new ToolTip(components);
+            ((System.ComponentModel.ISupportInitialize)dgvDati).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDati
+            // 
+            dgvDati.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDati.Location = new Point(121, 194);
+            dgvDati.Name = "dgvDati";
+            dgvDati.RowTemplate.Height = 25;
+            dgvDati.Size = new Size(509, 214);
+            dgvDati.TabIndex = 0;
+            // 
+            // btnPiloti
+            // 
+            btnPiloti.Location = new Point(82, 81);
+            btnPiloti.Name = "btnPiloti";
+            btnPiloti.Size = new Size(75, 23);
+            btnPiloti.TabIndex = 1;
+            btnPiloti.Text = "Piloti";
+            btnPiloti.UseVisualStyleBackColor = true;
+            btnPiloti.Click += btnPiloti_Click;
+            // 
+            // btnGare
+            // 
+            btnGare.Location = new Point(232, 81);
+            btnGare.Name = "btnGare";
+            btnGare.Size = new Size(75, 23);
+            btnGare.TabIndex = 2;
+            btnGare.Text = "Gare";
+            btnGare.UseVisualStyleBackColor = true;
+            btnGare.Click += btnGare_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnGare);
+            Controls.Add(btnPiloti);
+            Controls.Add(dgvDati);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvDati).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dgvDati;
+        private Button btnPiloti;
+        private Button btnGare;
+        private ToolTip toolTip1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,8 @@
+using System.Data.SQLite;
+using System.Data;
+using Microsoft.VisualBasic.ApplicationServices;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+
 namespace EsercizioAS2324._3G.GuardabascioRiccardo.PSDBPCTO
 {
     public partial class Form1 : Form
@@ -5,6 +10,50 @@ namespace EsercizioAS2324._3G.GuardabascioRiccardo.PSDBPCTO
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnPiloti_Click(object sender, EventArgs e)
+        {
+            string query = "";
+            string strConnessione = "";
+            strConnessione = "Data Source=" + @"C:\Users\PC\Desktop\EsercizioAS2324.3G.GuardabascioRiccardo.PSDBPCTO\DB\MotoGP.db" + ";Version=3;";
+            DataTable dtDati = new DataTable();
+            query = "SELECT * FROM Piloti ORDER BY Nome, Cognome";
+
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query, strConnessione);
+            try
+            {
+                da.Fill(dtDati);
+                dgvDati.DataSource = dtDati;
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show(exe.Message);
+            }
+        }
+
+        private void btnGare_Click(object sender, EventArgs e)
+        {
+            string query = "";
+            string strConnessione = "";
+            strConnessione = "Data Source=" + @"C:\Users\PC\Desktop\EsercizioAS2324.3G.GuardabascioRiccardo.PSDBPCTO\DB\MotoGP.db" + ";Version=3;";
+            DataTable dtDati = new DataTable();
+            query = "SELECT * FROM Gare";
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query, strConnessione);
+            try
+            {
+                da.Fill(dtDati);
+                dgvDati.DataSource = dtDati;
+            }
+            catch (Exception exe)
+            {
+                MessageBox.Show(exe.Message);
+            }
+        }
+
+        private void btnGare_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
